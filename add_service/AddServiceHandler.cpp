65cb5f96b3001443f3da273a1729d0ed68f920f6
David Harris <dhharris9@gmail.com>
2024-06-24 09:15:15 +0200

    implement AddServiceHandler

--- add_service/AddServiceHandler.cpp
+++ add_service/AddServiceHandler.cpp
@@ -1,1 +1,15 @@
 #include "AddServiceHandler.h"
+#include <iostream>
+
+// TODO: Figure out why buck doesn't find the cpp sources
+namespace AddService {
+int32_t AddServiceHandler::add(const int32_t n1, const int32_t n2) {
+  return n1 + n2;
+}
+int32_t AddServiceHandler::plus_plus(const int32_t num) {
+  return num + 1;
+}
+void AddServiceHandler::ping() {
+  std::cout << "ping" << std::endl;
+}
+} // namespace AddService
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
 #include "add_service/AddServiceHandler.h"
+#include <iostream>
 int main() {
+  auto server = std::make_unique<AddService::AddServiceHandler>();
   std::cout << "Hello from a C++ Buck2 program!" << std::endl;
 }
